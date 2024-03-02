bd42474578eec6a271140a94a35e538945822b7e
Manuel Sandi <manuelsandi97@hotmail.com>
2024-03-01 19:33:22 -0600

    Minificacion de archivos js + creacion de tarea "build" en gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 const { src, dest, watch, series } = require('gulp');
 
-// CSS y SASS
+// CSS y SASS, Js
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const cssnano = require('cssnano');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 // Imagenes
-//const imagemin = require('gulp-imagemin');
-//const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg,PNG}')
+        .pipe( avif( opciones ) )
+        .pipe( dest('build/img'))
+}
+
+//Css
 function css( done ) {
     src('src/scss/app.scss')
         .pipe( sourcemaps.init() )
@@ -23,38 +33,30 @@ function css( done ) {
     done();
 }
 
-// function imagenes() {
-//     return src('src/img/**/*')
-//         .pipe( imagemin({ optimizationLevel: 3 }) )
-//         .pipe( dest('build/img') )
-// }
-
-// function versionWebp() {
-//     const opciones = {
-//         quality: 50
-//     }
-//     return src('src/img/**/*.{png,jpg}')
-//         .pipe( webp( opciones ) )
-//         .pipe( dest('build/img') )
-// }
-function versionAvif() {
-    const opciones = {
-        quality: 50
-    }
-    return src('src/img/**/*.{png,jpg,PNG}')
-        .pipe( avif( opciones ) )
-        .pipe( dest('build/img'))
-}
-
 function dev() {
     watch( 'src/scss/**/*.scss', css );
     //watch( 'src/img/**/*', imagenes );
 }
 
+//Js
+function scripts(){
+    return src('js/*.js')  // Ruta de tus archivos JavaScript fuente
+    .pipe(concat('all-scripts.min.js'))  // Concatenar todos los archivos en uno solo
+    .pipe(uglify())  // Minificar el archivo resultante
+    .pipe(dest('build/js'));  // Directorio de destino
+}
+
+//Build
+function build(done) {
+    // Ejecutar las tareas de construcción en serie
+    series(versionAvif, scripts, css)(done);
+}
+
 
 exports.css = css;
 exports.dev = dev;
-//exports.imagenes = imagenes;
-//exports.versionWebp = versionWebp;
+exports.scripts = scripts;
+exports.build = build; 
+
 exports.versionAvif = versionAvif;
-exports.default = series(versionAvif, css, dev  );
+exports.default = series(build, dev);
